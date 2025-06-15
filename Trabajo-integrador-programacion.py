a0bf3c04b6b0d1697e27d00df0b8a8492509af08
Alex Austin <austinalex71@gmail.com>
2025-06-14 22:31:37 -0300

    arreglando errores de ortografia

--- Trabajo-integrador-programacion.py
+++ Trabajo-integrador-programacion.py
@@ -1,57 +1,58 @@
-class Nodo: # Se defino la clase para reprentar cada area en el organigrama
+class Nodo: # Se define la clase para representar cada área en el organigrama
 
-    def __init__(self, policia): # Cada nodo tiene su nombre (bombero, policia) y un diccionario de todos los bomberos
-        self.policia = policia # El nombre del nodo
+    def __init__(self, policia):  # Cada nodo tiene su nombre (bombero, policía) y un diccionario de todos los bomberos
+        self.policia = policia  # El nombre del nodo
         self.bomberos = {}  # Almacena nodos (Diccionario)
 
-    def agregar_funcion_de_las_direcciones(self, nodo):  # Funcion para agregar nodos
-        self.bomberos[nodo.policia] = nodo # Se usa el nombre del nodo como clave y se almancena
+    def agregar_funcion_de_las_direcciones(self, nodo):  # Función para agregar nodos
+        self.bomberos[nodo.policia] = nodo # Se usa el nombre del nodo como clave y se almacena
 
-    def buscar_dependencia(self, destino, camino=None): # Funcion para buscar un nodo en el arbol y devolver el camino desde la raiz
+    def buscar_dependencia(self, destino, camino=None): # Función para buscar un nodo en el árbol y devolver el camino desde la raíz
         if camino is None:
-            camino = [] # Se inicializa la lista de camino si no se ah proporcionado
+            camino = [] # Se inicializa la lista de camino si no se ha proporcionado
 
         camino.append(self) # Se agrega el nodo actual al camino
 
-        if self == destino: # SI el nodo actual es el nodo que estamos buscando retorna al camino recorrido hasta ahora
+        if self == destino:# Si el nodo actual es el nodo que estamos buscando, retorna el camino recorrido hasta ahora
             return camino
-        # Ciclo For para recorrer las dependencias de bomberos (recursivamente)
+        
+        # Ciclo for para recorrer las dependencias de bomberos (recursivamente)
         for bombero in self.bomberos.values(): # Se busca de manera recursiva en cada nodo, pasando el camino como una copia para no modificar la lista 
             camino_encontrado = bombero.buscar_dependencia(destino, camino[:])
-            if camino_encontrado: # Se encuentra el destino en la sub rama
-                return camino_encontrado # retorna el comino encontrado
+            if camino_encontrado: #: Se encuentra el destino en la sub-rama
+                return camino_encontrado # retorna el camino encontrado
+        return None # Si no se encuentra el nodo buscado, retorna None 
 
-        return None # Si no se encuentra el nodo buscardo, retorna en None
 
-    def calcular_grado_nodo(self): # Funcion para calcular el grado maximo del arbol
+    def calcular_grado_nodo(self): # Función para calcular el grado máximo del árbol
         return len(self.bomberos) # Retorna la cantidad de nodos
 
 
-    # Funcion para calcular el grado maximo del arbol 
+    # Función para calcular el grado máximo del árbol
     def calcular_grado_arbol(self):
         # Grado del árbol: máximo grado de todos los nodos
         grado_maximo = self.calcular_grado_nodo() # Se calcula el grado actual del nodo
         for bombero in self.bomberos.values(): # Se recorren todas las ramas y nodos
             grado_maximo = max(grado_maximo, bombero.calcular_grado_arbol())
-        return grado_maximo # Se retorna el grado maximo encontrado
+        return grado_maximo # Se retorna el grado máximo encontrado
 
-    # Funcion para calcular el peso del arbol
+   # Función para calcular el peso del árbol
     def calcular_peso_arbol(self):
         peso = 1 # Se cuenta desde el nodo actual
         for bombero in self.bomberos.values(): # Se recorren todos los nodos hoja
             peso += bombero.calcular_peso_arbol() # Se suman los nodos de las subramas
         return peso # Retorna el total encontrado
     
-    # Esta funcion genera e imprime una reprensetacion jerargica del arbol 
+    # Esta función genera e imprime una representación jerárquica del árbol
     def organigrama(self,indentado = 0): 
-        salto_de_la_rama = "  " * indentado # Crea espacios que se usara para identar y mejorar visualmetne cada nivel del arbol  
-        print(f"{salto_de_la_rama} |--- {self.policia} ") # Imprime el nombre de la unidad con identacion y un formato de rama de arbol
+        salto_de_la_rama = "  " * indentado # Crea espacios que se usará para indentar y mejorar visualmente cada nivel del árbol
+        print(f"{salto_de_la_rama} |--- {self.policia} ") # Imprime el nombre de la unidad con indentación y un formato de rama de árbol
         for depenecias in self.bomberos.values():
-            depenecias.organigrama(indentado + 1) # llama recursivamente al metodo organigrama para cada dependencia 
+            depenecias.organigrama(indentado + 1) # llama recursivamente al método organigrama para cada dependencia
         
         
 
-# Se crean todos los nodos que van a formar parte de nuestro arbol jerargico(organigrama) 
+# Se crean todos los nodos que van a formar parte de nuestro árbol jerárquico (organigrama) 
 bombero = Nodo("Policía Bombero")
 direccion = Nodo("Dirección de Bomberos")
 departamento_a = Nodo("DPTO. Coordinación Operativa")
@@ -73,48 +74,52 @@ seccion_g = Nodo("Sección Pericias")
 seccion_h = Nodo("Sección Informes Técnicos")
 
 
-# Agregamos y conectados la Raiz con las ramas, y las ramas con las hojas, armando asi nuestro arbol jerargico 
-# Se conecta la raiz con la Direccion Bomberos
+# Agregamos y conectamos la Raíz con las ramas, y las ramas con las hojas, armando así nuestro árbol jerárquico
 bombero.agregar_funcion_de_las_direcciones(direccion)
-# Se conectan los departamentos que depende de la Direccion Bomberos
+# Se conectan los departamentos que dependen de la Dirección de Bomberos
 direccion.agregar_funcion_de_las_direcciones(departamento_a)
 direccion.agregar_funcion_de_las_direcciones(departamento_b)
 direccion.agregar_funcion_de_las_direcciones(departamento_c)
-# Se conectan las Subdependencias que depende del Derpartamento Coordinacion operativa
+
+# Se conectan las Subdependencias que dependen del Departamento Coordinación Operativa
 departamento_a.agregar_funcion_de_las_direcciones(division_a)
 departamento_a.agregar_funcion_de_las_direcciones(seccion_a)
 departamento_a.agregar_funcion_de_las_direcciones(seccion_b)
-# Subdependencias del departamento Tecnico Administrativo
+
+# Subdependencias del Departamento Técnico Administrativo
 departamento_b.agregar_funcion_de_las_direcciones(seccion_c)
 departamento_b.agregar_funcion_de_las_direcciones(division_b)
-#Subdependencias del DUAR (departamento de unidades de alto riesgo)
+
+# Subdependencias del DUAR (Departamento de Unidades de Alto Riesgo)
 departamento_c.agregar_funcion_de_las_direcciones(division_c)
 departamento_c.agregar_funcion_de_las_direcciones(brigada_a)
 departamento_c.agregar_funcion_de_las_direcciones(brigada_b)
 departamento_c.agregar_funcion_de_las_direcciones(departamento_d)
-# Subdivisiones del servicio tecnico
+
+# Subdivisiones del servicio técnico
 seccion_c.agregar_funcion_de_las_direcciones(seccion_d)
 seccion_c.agregar_funcion_de_las_direcciones(seccion_e)
 seccion_c.agregar_funcion_de_las_direcciones(seccion_f)
-# Subdivisiones de la division de investigacion siniestrales
+
+# Subdivisiones de la División de Investigación Siniestrales
 division_b.agregar_funcion_de_las_direcciones(seccion_g)
 division_b.agregar_funcion_de_las_direcciones(seccion_h)
 
 
 # -------- IMPRESIONES --------
-print("\nDependencias de los  bomberos:")  # Imprime las dependencias desde la Raiz
+print("\nDependencias de los  bomberos:")  # Imprime las dependencias desde la Raíz
 for nodo in bombero.bomberos.values(): # 
     print(" >", nodo.policia) # 
 
-print("\nDependencias de la Dirección de Bomberos:") # Imprime los nodos de la Direccion bomberos
+print("\nDependencias de la Dirección de Bomberos:") # Imprime los nodos de la Dirección de Bomberos
 for nodo in direccion.bomberos.values():# 
     print("  >", nodo.policia) # 
 
-print("\nDependencias del DPTO. Coordinación Operativa:") # Imprime los nodos del Dpto, coordinacion operativa
+print("\nDependencias del DPTO. Coordinación Operativa:") # Imprime los nodos del Dpto. Coordinación Operativa
 for nodo in departamento_a.bomberos.values():
     print(" >", nodo.policia) #
 
-print("\nSubfunciones de Investigación Siniestro:") # Imprime los nodo de Investigaciones Siniestrales
+print("\nSubfunciones de Investigación Siniestro:") # Imprime los nodos de Investigaciones Siniestrales
 for nodo in division_b.bomberos.values():
     print(" >", nodo.policia) # 
 
@@ -123,18 +128,21 @@ for nodo in division_b.bomberos.values():
 grado = bombero.calcular_grado_arbol()
 peso = bombero.calcular_peso_arbol()
 
-print(f"\nEl grado maximo del árbol es: {grado}") # Imprimimos el Grado del arbol
-print(f"El peso del árbol es igual a: {peso}")# Imprimimos el Peso del arbol
+print(f"\nEl grado maximo del árbol es: {grado}") # Imprimimos el Grado del árbol
+print(f"El peso del árbol es igual a: {peso}") # Imprimimos el Peso del árbol
 
 
 # -------- BUSCAR CAMINO HASTA UN NODO --------
-camino = bombero.buscar_dependencia(seccion_h)  # Buscamos el recorrido del nodo_r
+camino = bombero.buscar_dependencia(seccion_h)  # Buscamos el recorrido hasta el nodo 'seccion_h'
 
 # -------- IMPRIME EL RECORRIDO --------
 print("\nCamino jerárquico hasta 'Sección Informes Técnicos':")
 
 for paso in camino:
-    print(" >", paso.policia)
+    if camino: 
+        print(" >", paso.policia)
+    else:
+        print("No se encontró la dependencia.")
 
 # -------- IMPRIME EL ORGANIGRAMA --------
 print("\nEl organigrama de la direccion de bomberos es: ")
